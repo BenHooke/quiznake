ad464f370af71fa1083de2e11513344fc560b7de
BenHooke <BenHooke73@live.ca>
2024-07-03 22:56:35 -0400

    Removed MappingProxyType for now and made first key in questions

--- questions.py
+++ questions.py
@@ -1,5 +1,25 @@
-from types import MappingProxyType
+# from types import MappingProxyType #Makes questions immutable to avoid errors
 
-questions = MappingProxyType({[
-    #add all commands with questions and answers here
-]})
+#template for copy/pasting
+
+    # '' : {
+    #     'question' : '',
+    #     'answer' : '',
+    #     'hint' : '',
+    #     'example' : '',
+    # }
+
+questions = {
+    'print' : {
+        'question' : 'What command would you use to display something to the user?',
+        'answer' : 'print()',
+        'hint' : "This is one of the first ones you learned. Think of the output as a message you're writing to a friend.",
+        'example' : 'Input: print("Hello Squirrel")\nOutput:>>>"Hello Squirrel"',
+    },
+    '' : {
+        'question' : '',
+        'answer' : '',
+        'hint' : '',
+        'example' : '',
+    }
+}
