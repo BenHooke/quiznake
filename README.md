e0166e4ff1e140747856ccd555fc7adb84a0efbb
BenHooke <BenHooke73@live.ca>
2024-07-03 22:22:45 -0400

    Added to readme file

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # quiznake
 Learning how to code by teaching code.
+
+
+I am trying to learn how to use Python so I thought it would be fun to make a learning tool and learn while building it.
--- python_quiz.py
+++ python_quiz.py
@@ -9,3 +9,24 @@ print('''        __________
   /  /____\\ /  / /_/ /__/ /_  /  /__________________________   
  /___________\\/_____//_____/ /_____________________________/   
                                                                ''')
+
+def start_quiz():
+    #welome user with breif instructions and a start command
+
+def get_question():
+    #start the quiz with a random question from question.py
+
+def get_hint():
+    #display a hint associated with list item in questions.py
+
+def get_user_guess():
+    #get user input to compare with 'answer' key value in questions.py
+
+def user_guess_result()
+    #choose whether to display 'Correct Answer' or 'Wrong Answer' message
+
+def show_example():
+    #display 'example' key value from questions.py if user_guess is correct
+
+def get_help():
+    #display a set of commands if user is stuck i.e. 'quit', 'skip' etc. until UI is made
